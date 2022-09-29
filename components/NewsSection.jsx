b9a1a77694adea43a4d1d156541a09cf67b6e514
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-09-29 20:49:58 +0700

    styckyu

--- components/NewsSection.jsx
+++ components/NewsSection.jsx
@@ -9,14 +9,16 @@ const NewsSection = () => {
   return (
     <div className="max-w-6xl mx-auto">
       <div className="flex flex-col md:flex-row-reverse">
-        <div className="basis-1/3">
+        <div className="basis-1/3 relative">
           <Discover />
         </div>
         <div className="border-b-2 border-gray-200 my-8" />
         <div className="flex-1">
           <CardNews />
           <CardNews />
+          <CardNews />
 
+          <CardNews />
           <CardNews />
 
           <CardNews />
